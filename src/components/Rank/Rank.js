a5d81faccaecc582eced38e167c4217f92f110f9
willianac <105891930+willianac@users.noreply.github.com>
2022-09-28 14:05:05 -0300

    updating rank

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = ({User}) => {
     const {name, entries} = User
     return (
         <div style={{textAlign : 'center', color : 'white', fontSize : '35px', fontFamily : 'Courier New'}}>
-            <div>{`${name}, your rank is...`}</div>
+            <div>{`${name}, your current entry count is...`}</div>
             <div>{`${entries}`}</div>
         </div>
     )
